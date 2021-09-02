a697822160ff34cf06bbdea65b3707c01d001139
arafathemal <arafatfaiyaz@gmail.com>
2021-09-02 13:13:02 +0600

    book-archive-4

--- index.js
+++ index.js
@@ -1,38 +1,40 @@
+const errorMessage = document.getElementById("errors");
 // search part
-const errorMessage = document.getElementById("no-result");
 const getSearchValue = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     searchField.value = "";
-    if (!searchText) {
+    // if empty search / searchText null
+    if (searchText === "") {
         errorMessage.innerText = "please write a book name!!"
-    }
-    else {
-        errorMessage.textContent = "";
-    }
+    };
     const url = `https://openlibrary.org/search.json?q=${searchText}`;
     fetch(url)
         .then(res => res.json())
-        .then(data => getBookData(data))
-
-}
+        .then(data => getBookData(data));
+};
 // search data load part
 const getBookData = data => {
-    const details = data.docs.slice(0,25);
-    console.log(details)
+    // when unexpected word use by search set error 
+    if (data.numFound === 0) {
+        errorMessage.innerText = 'no result found';
+    }
+    else {
+        errorMessage.innerText = "";
+    };
+    const details = data.docs.slice(0, 25);
     const divContainer = document.getElementById("div-container");
     divContainer.textContent = "";
     details.forEach(detail => {
-        console.log(detail)
         const div = document.createElement("div");
         div.classList.add("col");
         div.innerHTML = `
         <div class="card h-80">
-        <img h-50 src="https://covers.openlibrary.org/b/id/${detail.cover_i ? detail.cover_i :""}-L.jpg" class="card-img-top" alt="...">
+        <img h-50 src="https://covers.openlibrary.org/b/id/${detail.cover_i ? detail.cover_i : ""}-L.jpg" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title"><span class="text-primary">Book Name</span>: ${detail.title ? detail.title : "name not found"}</h5>
           <h5><span class="text-primary">Author Name</span>:${detail.author_name ? detail.author_name : "author name missing"}</h5>
-          <h5><span class="text-primary">Publisher</span>:${detail.publisher.slice(0,3)}</h5>
+          <h5><span class="text-primary">Publisher</span>:${detail.publisher.slice(0, 3)}</h5>
           
         </div>
         <div class="card-footer">
@@ -43,4 +45,4 @@ const getBookData = data => {
 
     });
 
-}
+};
